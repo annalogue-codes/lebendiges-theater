43b705161ce7e7fe9e895cf677100f1718099955
Annaluise Blume <hello@annalogue.codes>
2025-04-30 05:12:09 +0200

    Turn off debug mode.

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -14,10 +14,10 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-const debug = true
+const debug = false
 const keepScene = false
-const presentation = true
-const resume = true
+const presentation = false
+const resume = false
 
 function log <T>(s: any, content?: T): T | undefined {
 	if (debug) console.log(s)
